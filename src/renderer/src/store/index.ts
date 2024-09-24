e942d62131075fa343ca9e03aaf26c292891e336
victorqnk <victorqnk@mail.com>
2024-09-24 10:43:42 -0600

    add notes atoms

---
+++ src/renderer/src/store/index.ts
@@ -0,0 +1,20 @@
+import { NoteInfo } from '@shared/models'
+import { atom } from 'jotai'
+import { notesMock } from './mocks'
+
+export const notesAtom = atom<NoteInfo[]>(notesMock)
+
+export const selectedNoteIndexAtom = atom<number | null>(null)
+
+export const selectedNoteAtom = atom((get) => {
+  const notes = get(notesAtom)
+  const selectedNoteIndex = get(selectedNoteIndexAtom)
+
+  if (!selectedNoteIndex) return null
+  const selectedNote = notes[selectedNoteIndex]
+
+  return {
+    ...selectedNote,
+    content: `Hello from Note ${selectedNoteIndex}`
+  }
+})
